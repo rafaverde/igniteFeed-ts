8f87165f804a5f75fa6623e08d2775a7fda95e23
rafaverde <rafaverde@msn.com>
2024-08-23 16:53:12 -0300

    Comments and Avatar typescript ready. All components updated and working good.

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -4,7 +4,12 @@ import styles from "./Comment.module.css"
 import { Avatar } from "./Avatar"
 import { useState } from "react"
 
-export function Comment({ content, onDeleteComment }) {
+interface CommentProps {
+  content: string
+  onDeleteComment: (comment: string) => void
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
   const [likeCount, setLikeCount] = useState(0)
 
   function handleCommentDelete() {
@@ -22,6 +27,7 @@ export function Comment({ content, onDeleteComment }) {
       <Avatar
         hasBorder={false}
         src="https://randomuser.me/api/portraits/lego/1.jpg"
+        alt=""
       />
 
       <div className={styles.commentBox}>
